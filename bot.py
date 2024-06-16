31c3b84fd3052a104860026e0a949a151ebfe869
Ilham Ibrahimli <ibrahimli.ij@gmail.com>
2024-06-16 09:43:46 +0100

    final upload

--- bot.py
+++ bot.py
@@ -1,5 +1,68 @@
 import logic
-man = logic.FAQmanager()
+from telebot import TeleBot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import *
+import uuid
+import os
 
+man = logic.FAQmanager()
 man.load_file("FAQ.csv")
-print(man.similarity_search("Что делать, если товар пришел поврежденным?"))
+bot = TeleBot(TELEBOT_API)
+
+
+
+def gen_markup_for_text():
+        markup = InlineKeyboardMarkup()
+        markup.row_width = 1
+
+        markup.add(InlineKeyboardButton('Да', callback_data='transfer'),
+                   InlineKeyboardButton('Нет', callback_data='no_transfer'))
+        
+        return markup
+
+@bot.message_handler(content_types=["voice"])
+def audio_question(message):
+    filename = str(uuid.uuid4())
+    file_name_full='voice/'+filename+'.ogg'
+    file_name_full_converted = 'ready/'+filename+".wav"
+    file_info = bot.get_file(message.voice.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    with open(file_name_full, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    os.system("ffmpeg -i "+file_name_full+"  "+file_name_full_converted)
+
+    text = man.recognise_audio(file_name_full_converted)
+
+    os.remove(file_name_full)
+    os.remove(file_name_full_converted)
+    print(text)
+    text = man.similarity_search(text)
+    answer = man.parse_text(text)
+    if answer == "Либо ваш ответ слишком сложен для меня, либо я не понял, что вы сказали. Хотите поговорить с профессионалом?":
+        bot.send_message(message.chat.id,answer, reply_markup=gen_markup_for_text())
+    else:
+        bot.send_message(message.chat.id,answer)
+    
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+
+    if call.data == "transfer":
+        bot.send_message(call.message.chat.id, "Передача к профессионалу. Пожалуйста, подождите...")
+            
+    elif call.data == "no_transfer":
+        bot.send_message(call.message.chat.id,  "Повторите, пожалуйста, то, что вы хотели спросить.")
+
+
+
+@bot.message_handler(func=lambda message: True)
+def text_question(message):
+    text = man.similarity_search(message.text)
+    answer = man.parse_text(text)
+    bot.send_message(message.chat.id,answer)
+    if answer == "Либо ваш ответ слишком сложен для меня, либо я не понял, что вы сказали. Хотите поговорить с профессионалом?":
+        bot.send_message(message.chat.id,answer, reply_markup=gen_markup_for_text())
+    else:
+        bot.send_message(message.chat.id,answer)
+
+bot.infinity_polling()
--- logic.py
+++ logic.py
@@ -3,6 +3,9 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 import os
 import config
+import speech_recognition as sr
+
+
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = config.HUGGING_FACE_TOKEN
 class FAQmanager:
     def load_file(self,file):
@@ -12,7 +15,30 @@ class FAQmanager:
         self.db = FAISS.from_documents(self.document,self.embeddings)
 
     def similarity_search(self,query):
-        search = self.db.similarity_search(query)
+        search = self.db.similarity_search_with_score(query)
         print(type(search))
         return search
+    
+    def parse_text(self,text):
+        #
+        print(text)
+        closest_result = text[0][0].page_content
+        if text[0][1] <= 1.2:
+            qa_list = closest_result.split("\n")
+            answer = qa_list[1]
+            answer = answer[7:]
+            return answer
+        else:
+            return "Либо ваш ответ слишком сложен для меня, либо я не понял, что вы сказали. Хотите поговорить с профессионалом?"
+
+
+    def recognise_audio(self,path):
+        try:
+            r = sr.Recognizer()
+            with sr.AudioFile(path) as source:
+                audio_data = r.record(source)
+            text = r.recognize_google(audio_data, language="ru-RU")
+            return text
+        except:
+            return "transcription failed."
 
